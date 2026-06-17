1d76d10c2ee09ca318cc7dcc2cc53736f793a9c9
anuragsingh77 <squrllbites@gmail.com>
2026-06-17 19:47:15 +0530

    updated fotter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 function Footer() {
   return (
-   <footer className="bg-[#6B3E26] text-[#FFF8EE] mt-20">
+   <footer className="bg-[#000000] text-[#FFF8EE] mt-20">
 
   {/* TOP SECTION */}
   <div className="max-w-7xl mx-auto px-6 md:px-10 py-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-12">
